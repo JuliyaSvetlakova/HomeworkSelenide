10b847bc3ed72bc811acdecb30adbe197cac06e5
Juliya Svetlakova <9268385539@mail.ru>
2023-08-25 17:01:22 +0300

    Initial commit

---
+++ src/test/java/ru/netology/homeworkSelenide/HomeworkSelenideTest.java
@@ -0,0 +1,11 @@
+package ru.netology.homeworkSelenide;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class HomeworkSelenideTest {
+
+    public void shouldSubmitTheForm(){
+        open("http://localhost:9999");
+
+    }
+}
